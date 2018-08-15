b0cc7940c363303f74d537568311d9741b19c659
alper <alperkay@hotmail.com>
2018-08-15 10:26:09 +0200

    jwt token created on login

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://alperkay1:alperkay1@ds121182.mlab.com:21182/devconnector123'
+  mongoURI: 'mongodb://alperkay1:alperkay1@ds121182.mlab.com:21182/devconnector123',
+  secretOrKey: 'secret'
 }
